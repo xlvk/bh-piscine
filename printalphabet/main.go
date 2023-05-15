974a96e79c529b8a11e116a7559c19871c56e986
fatabbas <fatima77614@gmail.com>
2023-05-15 18:04:06 +0300

    2 pt.1

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,9 +1,9 @@
 package main
 
-import "fmt"
+import "github.com/01-edu/z01"
 func main() {
-for i:='a'; i<= 'z'; i++ {
-	fmt.Printf("%c",i)
-}
+	for i:='a'; i<= 'z'; i++ {
+		z01.PrintRune(i)
+	}
 
 }
