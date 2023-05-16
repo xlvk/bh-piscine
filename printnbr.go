d3015921fa9ef78ed9dacba9dabeb139b0420c58
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-16 23:51:55 +0000

    Update 'printnbr.go'

--- printnbr.go
+++ printnbr.go
@@ -5,22 +5,18 @@ import "github.com/01-edu/z01"
 func printInt(n int) {
     digits := []byte{}
     negative := false
-
     if n < 0 {
         negative = true
         n = -n
     }
-
     for n > 0 {
         digit := n % 10
         digits = append([]byte{'0' + byte(digit)}, digits...)
         n /= 10
     }
-
     if negative {
         fmt.Print("-")
     }
-
     if len(digits) == 0 {
         fmt.Print("0")
     } else {
