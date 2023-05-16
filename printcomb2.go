b28ab30bed035cf432a2f356e61d891de7ec1ac4
fatabbas <fatima77614@aabas.com>
2023-05-16 20:34:32 +0300

    q2, pt.7

--- printcomb2.go
+++ printcomb2.go
@@ -5,11 +5,11 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '8'; j++ {
-			z01.PrintRune(i)
-			z01.PrintRune(j)
-			z01.PrintRune(' ')
 			for k := '0'; k <= '9'; k++ {
 				for o := '0'; o <= '8'; o++ {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
 					z01.PrintRune(k)
 					z01.PrintRune(o)
 					if i == '9' && j == '8' && k == '9' && o == '9' {
