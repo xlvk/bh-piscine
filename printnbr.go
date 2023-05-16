4c2440d02949a79be3db7d11acc3cf788cd460ae
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-16 23:47:25 +0000

    Update 'printnbr.go'

--- printnbr.go
+++ printnbr.go
@@ -2,14 +2,28 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintNbr(n int64) {
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
-	}
-	if n >= 10 {
-		PrintNbr(n / 10)
-	}
-	digit := '0' + n%10
-	z01.PrintRune(digit)
+func printInt(n int) {
+    digits := []byte{}
+    negative := false
+
+    if n < 0 {
+        negative = true
+        n = -n
+    }
+
+    for n > 0 {
+        digit := n % 10
+        digits = append([]byte{'0' + byte(digit)}, digits...)
+        n /= 10
+    }
+
+    if negative {
+        fmt.Print("-")
+    }
+
+    if len(digits) == 0 {
+        fmt.Print("0")
+    } else {
+        fmt.Print(string(digits))
+    }
 }
