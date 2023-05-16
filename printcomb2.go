0d3503f999623c670cc1d1380ea5c9dbd8bf73ac
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-16 22:34:50 +0000

    Update 'printcomb2.go'

--- printcomb2.go
+++ printcomb2.go
@@ -1,32 +1,29 @@
 package piscine
-
 import "github.com/01-edu/z01"
-
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			for k := '0'; k <= '9'; k++ {
-				if i == '0' && j == '0' && k == '0' {
-					for o := '1'; o <= '9'; o++ {
-						z01.PrintRune(i)
-						z01.PrintRune(j)
-						z01.PrintRune(' ')
-						z01.PrintRune(k)
-						z01.PrintRune(o)
+			for k := '1'; k <= '5'; k++ {
+				for o := '1'; o <= '9'; o++ {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(o)
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+			for k := '6'; k <= '9'; k++ {
+				for o := '0'; o <= '9'; o++ {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(o)
+					if !(i == '9' && j == '8' && k == '9' && o == '9') {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
-					}
-				} else {
-					for o := '0'; o <= '9'; o++ {
-						z01.PrintRune(i)
-						z01.PrintRune(j)
-						z01.PrintRune(' ')
-						z01.PrintRune(k)
-						z01.PrintRune(o)
-						if !(i == '9' && j == '8' && k == '9' && o == '9') {
-							z01.PrintRune(',')
-							z01.PrintRune(' ')
-						}
 					}
 				}
 			}
