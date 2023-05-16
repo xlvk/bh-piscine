c09fc0ca1a8473f71b093541bab15e08c7827b6f
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-16 23:30:23 +0000

    Add 'printnbr.go'

---
+++ printnbr.go
@@ -0,0 +1,15 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	if n >= 10 {
+		PrintNbr(n / 10)
+	}
+	digit := '0' + rune(n%10)
+	z01.PrintRune(digit)
+}
