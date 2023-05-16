9281618e70bb258142b742d8f7a9985eb91cab5a
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-16 22:17:04 +0000

    Update 'printcomb2.go'

--- printcomb2.go
+++ printcomb2.go
@@ -5,29 +5,27 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			for k := j; k <= '9'; k++ {
+			for k := '0'; k <= '9'; k++ {
 				if i == '0' && j == '0' && k == '0' {
-					for o := '0'; o <= '9'; o++ {
-					count := '1'
-					count++
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(' ')
-					z01.PrintRune(k)
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
-					} else {
-					for o := '0'; o <= '9'; o++ {
+					for o := '1'; o <= '9'; o++ {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
 						z01.PrintRune(' ')
-						z01.PrintRune(k)	
-						z01.PrintRune(o)
-						if i == '9' && j == '8' && k == '9' && o == '9' {
-							break
-						} else {
-							z01.PrintRune(',')
+						z01.PrintRune(k)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					} else {
+						for o := '0'; o <= '9'; o++ {
+							z01.PrintRune(i)
+							z01.PrintRune(j)
 							z01.PrintRune(' ')
+							z01.PrintRune(k)	
+							z01.PrintRune(o)
+							if i == '9' && j == '8' && k == '9' && o == '9' {
+								break
+							} else {
+								z01.PrintRune(',')
+								z01.PrintRune(' ')
 							}
 						}
 					}
