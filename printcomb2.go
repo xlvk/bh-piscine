f338b7631613ea7ae54236caa6fa8a754fd2ff2c
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-16 21:40:31 +0000

    Update 'printcomb2.go'

--- printcomb2.go
+++ printcomb2.go
@@ -6,12 +6,18 @@ func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := j; k <= '9'; k++ {
-				for o := j + 1; o <= '9'; o++ {
+				for o := '0'; o <= '9'; o++ {
+					count := '0'
+					count++
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(' ')
 					z01.PrintRune(k)
-					z01.PrintRune(o)
+					if count == 1 {
+						z01.PrintRune(o+1)
+					} else {
+						z01.PrintRune(o)
+					}
 					if i == '9' && j == '8' && k == '9' && o == '9' {
 						break
 					} else {
