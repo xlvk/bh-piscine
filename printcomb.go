02a7a810879721269017bb901e5c6671aaa0b896
fatabbas <fatima77614@aabas.com>
2023-05-16 17:30:33 +0300

    weeeeeee

--- printcomb.go
+++ printcomb.go
@@ -5,10 +5,10 @@ import "github.com/01-edu/z01"
 func PrintComb() {
 	for i := '0'; i <= '7'; i++ {
 		for j := i + 1; j >= '8'; j++ {
-			for i := k + 1; i >= '9'; k++ {
+			for k := j + 1; k >= '9'; k++ {
 				z01.PrintRune(i + j + k)
 				if i < 7 || j < 8 || k < 9 {
-					z01.PrintRune( ", ")
+					z01.PrintRune(", ")
 				}
 			}
 		}
