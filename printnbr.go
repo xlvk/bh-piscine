cb3c454ae8327ea1cd8c0f6d7419ffb29bf67f95
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-16 23:43:05 +0000

    Update 'printnbr.go'

--- printnbr.go
+++ printnbr.go
@@ -2,8 +2,14 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintNbr(n int) {
-	for _, ch := n {
-		z01.PrintRune(ch)
+func PrintNbr(n int64) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
 	}
+	if n >= 10 {
+		PrintNbr(n / 10)
+	}
+	digit := '0' + n%10
+	z01.PrintRune(digit)
 }
