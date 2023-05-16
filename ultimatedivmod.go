09b6944b7761236994c756ad788e9519ef3a6c32
fatabbas <fatima77614@aabas.com>
2023-05-16 19:48:16 +0300

    q3, pt.4

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,8 +1,8 @@
 package piscine
 
-func UltimateDivMod(a *int, b *int, div int, mod int) {
-	div = *a / *b
-	mod = *a % *b
+func UltimateDivMod(a *int, b *int, div int) {
+	div := *a / *b
+	mod := *a % *b
 	*a = div
 	*b = mod
 }
