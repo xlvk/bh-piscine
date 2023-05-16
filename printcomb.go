95298c083a0a0f94c6258cd184828e491dab790e
fatabbas <fatima77614@aabas.com>
2023-05-16 18:12:11 +0300

    weeeeeee

--- printcomb.go
+++ printcomb.go
@@ -9,7 +9,7 @@ func PrintComb() {
 				z01.PrintRune(i)
 				z01.PrintRune(j)
 				z01.PrintRune(k)
-				if i != '7' && k != '8' && j != '9' {			
+				if i != '7' && k != '8' && j != '9' {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
