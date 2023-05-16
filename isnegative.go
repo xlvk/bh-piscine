0cf5535489760890ce75b37144129c0632024195
fatabbas <fatima77614@aabas.com>
2023-05-16 16:59:05 +0300

    weeeeeeeeeeeeeeeeeeee

--- isnegative.go
+++ isnegative.go
@@ -4,15 +4,9 @@ import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb < 0 {
-		z01.PrintRune("T")
+		z01.PrintRune('T')
+	} else {
+		z01.PrintRune('F')
 	}
-	if nb >= 0 {
-		z01.PrintRune("F")
-	}
-}
-
-func main() {
-	IsNegative(1)
-	IsNegative(0)
-	IsNegative(-1)
+	z01.PrintRune('\n')
 }
