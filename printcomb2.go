833e7876ae144aeb7bc16aec99d6b921fadf7af7
fatabbas <fatima77614@aabas.com>
2023-05-16 20:20:03 +0300

    q2, pt.7

---
+++ printcomb2.go
@@ -0,0 +1,19 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for i := 0o0; i <= 98; i++ {
+		for j := 0o0; j <= 99; j++ {
+			z01.PrintRune(i)
+			z01.PrintRune(j)
+			if i == 98 && j == 99 {
+				break
+			} else {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+		}
+	}
+	z01.PrintRune('$')
+}
