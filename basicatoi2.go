5da919910637e46daa9f640336e922ec9a91a07b
Fatima Abbas <fatabbas@LAPT-0020.reboot.internal>
2023-05-17 19:59:39 +0300

    q3, pt.10

---
+++ basicatoi2.go
@@ -0,0 +1,17 @@
+package piscine
+
+func BasicAtoi2(s string) int {
+	if len(s) == o {
+		return 0
+	}
+	num := 0
+	for _, a := range s {
+		if a < '0' || a > '9' {
+			return 0
+		}
+		x := int(a - '0')
+		num = num*10 + x
+	}
+	return num
+
+}
