ae8fbd7d5f64fa3f33b40770b9464524a11977a2
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-17 22:37:11 +0000

    printnbr.go
    printnbr.go

---
+++ printnbr.go
@@ -0,0 +1,25 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	digits := make([]int, 0)
+	for n > 0 {
+		digit := n % 10
+		digits = append(digits, digit)
+		n /= 10
+	}
+	for i := len(digits) - 1; i >= 0; i-- {
+		z01.PrintRune(rune(digits[i]) + '0')
+	}
+}
