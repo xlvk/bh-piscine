8ee93f654e40e6866af7fb0a3ee4b1ddc9ee0fe6
fatabbas <fatima77614@gmail.com>
2023-05-20 17:52:04 +0300

    Q4, pt.2

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -84,7 +84,6 @@ func IterativeFactorial(nb int) int {
 		if nb == 25 {
 			return (25 * 24 * 23 * 22 * 21 * 20 * 19 * 18 * 17 * 16 * 15 * 14 * 13 * 12 * 11 * 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2)
 		}
-		
 	}
 	return result
 }
