0476241b6f7bc80969951495a2247763685a8d1d
fatabbas <fatima77614@gmail.com>
2023-05-20 16:51:59 +0300

    Q3, pt.4

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,14 +1,15 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func IterativeFactorial(nb int) int {
-if nb < 0 {
-	return 0
-}
-wee := 1
-for i := 1; i <= nb: i++ {
-wee = wee*i
-}
-return wee
-}
+	if nb < 0 {
+		return 0
+	}
+	wee := 1
+	for i := 1; i <= nb: i++ {
+			if wee > (i<<31-1)/i {
+				return 0
+			}
+		wee = wee*i
+	}
+	return wee
+	}
