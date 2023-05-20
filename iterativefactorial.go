21c7d38c121c8279208e10ae48c464c7c5c28107
fatabbas <fatima77614@gmail.com>
2023-05-20 17:35:57 +0300

    Q3, pt.4

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,10 +2,10 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	if nb > 25 {
+	if nb < 0 || nb > 25 {
 		return 0
 	}
-	if nb <= 0 {
+	if nb == 0 {
 		return 1
 	}
 	if nb <= 25 && nb > 0 {
