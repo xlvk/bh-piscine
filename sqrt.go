e9d34cb33cc247bf5aa98c163b2a1f041949146c
fatabbas <fatima77614@gmail.com>
2023-05-20 19:22:20 +0300

    Q4, pt.6

--- sqrt.go
+++ sqrt.go
@@ -4,8 +4,18 @@ import "math"
 
 func Sqrt(nb int) int {
 	result := int(math.Sqqrt(float64(nb)))
-	if result*result == nb {
-		return result
+	start := 1
+	end := nb 
+	for start <= end {
+		mid := (start + end) / 2
+		square := mid * mid
+		if square == nb {
+			return mid
+		} else if square < nb {
+			start = mid + 1
+		} else {
+			mid - 1
+		}
 	}
 	return 0
 }
