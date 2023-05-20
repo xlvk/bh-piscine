46dc37355ba2fb90d108add825949d909ae977f0
fatabbas <fatima77614@gmail.com>
2023-05-20 20:15:45 +0300

    Q4, pt.8

--- findnextprime.go
+++ findnextprime.go
@@ -1,23 +1,8 @@
 package piscine
 
-func IsItPrime(nb int) bool {
-	if nb <= 1 {
-		return false
-	}
-	for i := 2; i*i <= nb; i++ {
-		if nb%i == 0 {
-			return false
-		}
-	}
-	return true
-}
-
 func FindNextPrime(nb int) bool {
-	if nb < 2 {
-		return 2
-	}
-	for i := nb + 1; ; i++ {
-		if IsItPrime(i) {
+	for i := nb; ; i++ {
+		if IsItPrime(i) == true {
 			prime := i
 		}
 	}
