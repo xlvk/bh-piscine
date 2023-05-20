f036808d44ff5e93cc9f497ec6c97b08b7ab43e0
fatabbas <fatima77614@gmail.com>
2023-05-20 18:31:41 +0300

    Q4, pt.4

--- recursivepower.go
+++ recursivepower.go
@@ -1,14 +1,11 @@
 package piscine
 
 func RecursivePower(nb int, power int) int {
-	result := 1
-	if power < 0 {
-		return 0
-	}
 	if power == 0 {
 		return 1
 	}
 	if power > 0 {
 		return nb * RecursivePower(nb, power-1)
 	}
+	return 0
 }
