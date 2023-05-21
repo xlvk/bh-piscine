3695b8d2a3a188fcdeaf03f3c389cfdde2b61aa9
fatabbas <fatima77614@gmail.com>
2023-05-21 18:48:01 +0300

    Q.4 pt.9

---
+++ eightqueens.go
@@ -0,0 +1,83 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+const n = 8
+
+var (
+	board     [n][n]bool
+	solutions [][]int
+)
+
+func EightQueens() {
+	solutions = make([][]int, 0)
+	placeQueens(0)
+	printSolutions()
+}
+
+func placeQueens(col int) {
+	if col == n {
+		saveSolution()
+		return
+	}
+
+	for row := 0; row < n; row++ {
+		if isSafe(row, col) {
+			board[row][col] = true
+			placeQueens(col + 1)
+			board[row][col] = false
+		}
+	}
+}
+
+func isSafe(row, col int) bool {
+	// Check row and column
+	for i := 0; i < col; i++ {
+		if board[row][i] {
+			return false
+		}
+	}
+
+	// Check upper diagonal
+	for i, j := row-1, col-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
+		if board[i][j] {
+			return false
+		}
+	}
+
+	// Check lower diagonal
+	for i, j := row+1, col-1; i < n && j >= 0; i, j = i+1, j-1 {
+		if board[i][j] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func saveSolution() {
+	solution := make([]int, 0)
+	for col := 0; col < n; col++ {
+		for row := 0; row < n; row++ {
+			if board[row][col] {
+				solution = append(solution, row+1)
+			}
+		}
+	}
+	solutions = append(solutions, solution)
+}
+
+func printSolutions() {
+	for _, solution := range solutions {
+		for _, pos := range solution {
+			z01.PrintRune(rune(pos + '0'))
+		}
+		z01.PrintRune('\n')
+	}
+}
+
+func main() {
+	EightQueens()
+}
