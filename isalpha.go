1d75a3a7efab31bae12450841bb166f1b9c8b1de
fatabbas <fatima77614@gmail.com>
2023-05-21 20:06:34 +0300

    Q.5 pt.10

--- isalpha.go
+++ isalpha.go
@@ -2,9 +2,16 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, w := range s {
-		if w == ' ' {
+		if isLte(w) == false {
 			return false
 		}
 	}
 	return true
+}
+
+func isLte(woo rune) bool {
+	if woo == ' ' {
+		return false
+	}
+	return (woo >= 'A' && woo <= 'Z') || (woo >= 'a' && woo <= 'z') || (woo >= '0' && woo <= '9')
 }
