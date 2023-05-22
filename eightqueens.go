befcb8311d4b103ca2eaaf892839f737d0e6978c
fatabbas <fatima77614@gmail.com>
2023-05-22 18:36:40 +0300

    wwwwwwwwwwwwww

--- eightqueens.go
+++ eightqueens.go
@@ -4,82 +4,37 @@ import (
 	"github.com/01-edu/z01"
 )
 
-const n = 8
-
-func EightQueens() {
-	solutions := make([][]int, 0)
-	board := [n][n]bool{}
-	col := 0
-
-	var placeQueens func(col int, board [n][n]bool)
-	placeQueens = func(col int, board [n][n]bool) {
-		if col == n {
-			saveSolution(board, &solutions)
-			return
-		}
-
-		for row := 0; row < n; row++ {
-			if isSafe(row, col, board) {
-				board[row][col] = true
-				placeQueens(col+1, board)
-				board[row][col] = false
-			}
-		}
-	}
-
-	placeQueens(col, board)
-	printSolutions(solutions)
-	z01.Flush()
+func main() {
+	EightQueens()
 }
 
-func isSafe(row, col int, board [n][n]bool) bool {
-	// Check row
-	for i := 0; i < col; i++ {
-		if board[row][i] {
+func IsSafe(qn int, rp int, pos [8]int) bool {
+	for i := 0; i < qn; i++ {
+		t := pos[i]
+		if t == rp || t == rp-(qn-i) || t == rp+(qn-i) {
 			return false
 		}
 	}
-
-	// Check upper diagonal
-	for i, j := row-1, col-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
-		if board[i][j] {
-			return false
-		}
-	}
-
-	// Check lower diagonal
-	for i, j := row+1, col-1; i < n && j >= 0; i, j = i+1, j-1 {
-		if board[i][j] {
-			return false
-		}
-	}
-
 	return true
 }
 
-func saveSolution(board [n][n]bool, solutions *[][]int) {
-	solution := make([]int, 0)
-	for col := 0; col < n; col++ {
-		for row := 0; row < n; row++ {
-			if board[row][col] {
-				solution = append(solution, row+1)
-			}
-		}
-	}
-	*solutions = append(*solutions, solution)
-}
-
-func printSolutions(solutions [][]int) {
-	for _, solution := range solutions {
-		for _, pos := range solution {
-			if pos >= 1 && pos <= 8 {
-				z01.PrintRune(rune(pos + '0'))
-			}
+func solve(nb int, pos [8]int) {
+	if nb == 8 {
+		for i := 0; i < 8; i++ {
+			z01.PrintRune(rune(pos[i] + '1'))
 		}
 		z01.PrintRune('\n')
+	} else {
+		for i := 0; i < 8; i++ {
+			if IsSafe(nb, i, pos) {
+				pos[nb] = i
+				solve(nb+1, pos)
+			}
+		}
 	}
 }
 
-func main() {
-	EightQueens()
+func EightQueens() {
+	pos := [8]int{0, 0, 0, 0, 0, 0, 0}
+	solve(0, pos)
 }
