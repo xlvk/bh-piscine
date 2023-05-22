a8de5aec8aaec79be04d189c4870dfc768b89540
fatabbas <fatima77614@gmail.com>
2023-05-22 16:21:16 +0300

    Q.5 pt.12

--- isprintable.go
+++ isprintable.go
@@ -1,17 +1,14 @@
 package piscine
 
 func IsPrintable(s string) bool {
-	for _, w := range s {
-		if isL(w) == false {
+	for _, r := range s {
+		if !isPrintabe(r) {
 			return false
 		}
 	}
 	return true
 }
 
-func isL(woo rune) bool {
-	if woo == '\n' {
-		return false
-	}
-	return (woo >= '0' && woo <= '9')
+func isPrintabe(r rune) bool {
+	return !(r == '\n' || (r >= '0' && r <= '9'))
 }
