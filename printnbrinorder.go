470171935d1ccd505e7ff28e3fab33f804ba8303
fatabbas <fatima77614@gmail.com>
2023-05-22 17:15:02 +0300

    Q.5 pt.15

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,11 +3,44 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
-	z01.PrintRune('1')
-	z01.PrintRune('2')
-	z01.PrintRune('3')
-	z01.PrintRune('0')
-	z01.PrintRune('1')
-	z01.PrintRune('2')
-	z01.PrintRune('3')
+	if n == 0 {
+		z01.PrintRune(0)
+		return
+	}
+
+	digits := []int{}
+	for n > 0 {
+		digit := n % 10
+		digits = append(digits, digit)
+		n /= 10
+	}
+
+	reverseSlice(digits)
+
+	for _, digi := range bubbleSort(digits) {
+		z01.PrintRune(digi)
+	}
+}
+
+func reverseSlice(slice []int) {
+	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+}
+
+func bubbleSort(slice []int) []int {
+	length := len(slice)
+	swapped := true
+
+	for swapped {
+		swapped = false
+		for i := 0; i < length-1; i++ {
+			if slice[i] > slice[i+1] {
+				slice[i], slice[i+1] = slice[i+1], slice[i]
+				swapped = true
+			}
+		}
+	}
+
+	return slice
 }
