9a410fd62c1a16a7e0986419cd23d554b4783b38
fatabbas <fatima77614@gmail.com>
2023-05-22 19:04:43 +0300

    Q.5 pt.16

--- trimatoi.go
+++ trimatoi.go
@@ -1,26 +1,37 @@
 package piscine
 
-func TrimAtoi(s string) int {
-	if len(s) == 0 {
-		return 0
-	}
-	sign := 1
-	result := 0
-	if s[0] == '+' || s[0] == '-' {
-		if s[0] == '-' {
-			sign = -1
+func TrimAtoi(w string) int {
+	size := 0
+	ind := '0'
+	var s string
+	for p := range w {
+		if rune(w[p]) >= '0' && rune(w[p]) <= '9' {
+			break
 		}
-		s = s[1:]
-	}
-	for _, ch := range s {
-		if ch < '0' || ch > '9' {
-			b := 0
-			for i := 0; i < int(ch-'0'); i++ {
-				b++
-			}
-			result = result*10 + b
+		if rune(w[p]) == '+' {
+			ind = '+'
+			break
+		}
+		if rune(w[p]) == '-' {
+			ind = '-'
+			break
 		}
 	}
-	result *= sign
-	return result
+	for p := range w {
+		if rune(w[p]) >= '0' && rune(w[p]) <= '9' {
+			s += string(w[p])
+			size++
+		}
+	}
+	d := 1
+	num := 0
+	for i := size - 1; i >= 0; i-- {
+		num += d * int(s[i]-'0')
+		d *= 10
+	}
+	if ind == '0' || ind == '+' {
+		return num
+	} else {
+		return -num
+	}
 }
