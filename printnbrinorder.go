565b5d416f3728dd556bde3430172321c7dd888b
fatabbas <fatima77614@gmail.com>
2023-05-22 17:11:51 +0300

    Q.5 pt.15

---
+++ printnbrinorder.go
@@ -0,0 +1,13 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	z01.PrintRune('1')
+	z01.PrintRune('2')
+	z01.PrintRune('3')
+	z01.PrintRune('0')
+	z01.PrintRune('1')
+	z01.PrintRune('2')
+	z01.PrintRune('3')
+}
