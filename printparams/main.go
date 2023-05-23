9ea02bd9172b34679ccc728327a19bde24983bb4
fatabbas <fatima77614@gmail.com>
2023-05-23 16:28:13 +0300

    Q.6, pt.2

---
+++ printparams/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	wee := os.Args[1:]
+	runes := []rune(wee)
+	for _, word := range runes {
+		if word == '.' || word == '/' {
+		} else {
+			z01.PrintRune(word)
+		}
+	}
+	z01.PrintRune('\n')
+}
