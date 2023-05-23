e232820725425a645f70d9305860d5ba9469df07
fatabbas <fatima77614@gmail.com>
2023-05-23 17:31:55 +0300

    Q.6, pt.4

--- sortparams/main.go
+++ sortparams/main.go
@@ -8,14 +8,14 @@ import (
 
 func main() {
 	n := len(os.Args)
-	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-i-1; j++ {
+	for i := 1; i < n-1; i++ {
+		for j := 1; j < n-i-1; j++ {
 			if os.Args[j] > os.Args[j+1] {
 				os.Args[j], os.Args[j+1] = os.Args[j+1], os.Args[j]
 			}
 		}
 	}
-	for i := len(os.Args); i < 1; i++ {
+	for i := 1; i < len(os.Args); i++ {
 		wee := os.Args[i]
 		for _, word := range wee {
 			z01.PrintRune(word)
