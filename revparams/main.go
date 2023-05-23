a70c7c6bcb41c475652d76171eac2e17128960e3
fatabbas <fatima77614@gmail.com>
2023-05-23 17:12:49 +0300

    Q.6, pt.3

---
+++ revparams/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	for i := len(os.Args)-1; i > 1; i-- {
+		wee := os.Args[i]
+		for _, word := range wee {
+			z01.PrintRune(word)
+		}
+		z01.PrintRune('\n')
+	}
+}
