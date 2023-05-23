7fe6f53e2dd00ec0fe4b7b81f9df22bcb034bb25
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-23 20:52:56 +0000

    Update 'nbrconvertalpha/main.go'

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -9,30 +9,26 @@ import (
 func main() {
 	arguments := os.Args[1:]
 	caps := false
-	for _, v := range arguments {
-		if v == "--upper" {
-			caps = true
-		}
+
+	if len(arguments) > 0 && arguments[0] == "--upper" {
+		caps = true
+		arguments = arguments[1:]
 	}
+
 	for _, arg := range arguments {
-		numv := 0
 		for _, c := range arg {
-			if c >= '0' && c <= '9' {
-				numv = numv*10 + int(c-'0')
+			if c < '0' || c > '9' {
+				z01.PrintRune(' ')
+				break
 			}
-		}
-		if numv >= 1 && numv <= 26 {
-			if caps == false {
-				z01.PrintRune(rune(numv - 1 + 'a'))
-			} else {
-				z01.PrintRune(rune(numv - 1 + 'A'))
+			numv := int(c - '0')
+			if numv >= 1 && numv <= 26 {
+				if caps {
+					z01.PrintRune(rune(numv - 1 + 'A'))
+				} else {
+					z01.PrintRune(rune(numv - 1 + 'a'))
+				}
 			}
-			z01.PrintRune(' ')
-		} else {
-			for _, c := range arg {
-				z01.PrintRune(c)
-			}
-			z01.PrintRune(' ')
 		}
 	}
 	z01.PrintRune('\n')
