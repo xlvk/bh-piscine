eb8db1c176812c2eaa91dab1bb1ce9ad6e360ca1
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-23 22:10:32 +0000

    Update 'flags/main.go'

--- flags/main.go
+++ flags/main.go
@@ -7,87 +7,69 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func main() {
-	args := os.Args
-	args = args[1:]
-	len := 0
-	for i := range args {
-		len = i + 1
+func order(s string) {
+	var a [5000]int
+	for _, c := range s {
+		a[int(c)]++
 	}
-	if len == 0 || args[0] == "-h" || args[0] == "--help" {
-		fmt.Print("--insert\n  -i\n\t This flag inserts the string into the string passed as argument.\n--order\n  -o\n\t This flag will behave like a boolean, if it is called it will order the argument.\n")
-	} else {
-		var runes []rune
-		order := false
-		order_prev := false
-		insert := false
-		for _, arg := range args {
-			length := 0
-			for j := range arg {
-				length = j + 1
-			}
-			if length > 9 && arg[:9] == "--insert=" {
-				runes_temp := []rune(arg[9:])
-				for i := 0; i < length-9; i++ {
-					runes = append(runes, runes_temp[i])
-				}
-				insert = true
-			} else if length > 3 && arg[:3] == "-i=" {
-				runes_temp := []rune(arg[3:])
-				for i := 0; i < length-3; i++ {
-					runes = append(runes, runes_temp[i])
-				}
-				insert = true
-			} else if (length == 7 && arg[:7] == "--order") || (length == 2 && arg[:2] == "-o") {
-				order = true
-				order_prev = true
-				continue
-			} else if order_prev {
-				runes_temp := []rune(arg)
-				for i := range runes_temp {
-					runes = append(runes, runes_temp[i])
-				}
-			} else {
-				runes_print := []rune(arg)
-				if order {
-					for i := range runes_print {
-						runes = append(runes, runes_print[i])
-					}
-				}
-				for i := range runes_print {
-					z01.PrintRune(runes_print[i])
-				}
-			}
-			order_prev = false
+	for i, c := range a {
+		for c > 0 {
+			z01.PrintRune(rune(i))
+			c--
 		}
-		if order {
-			Order(runes)
-		}
-		if order || insert {
-			for i := range runes {
-				z01.PrintRune(runes[i])
-			}
-		}
-		z01.PrintRune(10)
 	}
+	z01.PrintRune('\n')
 }
 
-func Order(array []rune) {
-	len := 0
-	for index := range array {
-		len = index + 1
+func insert(str1 string, str2 string) string {
+	return str1 + str2
+}
+
+func help() {
+	fmt.Println("--insert")
+	fmt.Println("  -i")
+	fmt.Println("	 This flag inserts the string into the string passed as argument.")
+	fmt.Println("--order")
+	fmt.Println("  -o")
+	fmt.Println("	 This flag will behave like a boolean, if it is called it will order the argument.")
+}
+
+func main() {
+	args := os.Args[1:]
+	ln := -1
+	for i := range args {
+		ln = i
 	}
-	sorted := false
-	for !sorted {
-		count := 0
-		for i := 1; i < len; i++ {
-			if array[i] < array[i-1] {
-				array[i], array[i-1] = array[i-1], array[i]
-				count++
+	if ln != -1 {
+		if args[0] == "-h" || args[0] == "--help" {
+			help()
+		} else if args[0] == "--order" || args[0] == "-o" {
+			order(args[1])
+		} else if args[0][0:3] == "--i" || args[0][0:2] == "-i" {
+			if ln < 2 {
+				if args[0][0:3] == "--i" {
+					fmt.Println(insert(args[1], args[0][9:]))
+				} else {
+					fmt.Println(insert(args[1], args[0][3:]))
+				}
+			} else {
+				ele := ""
+				for i := 1; i <= ln; i++ {
+					if args[i] != "-o" && args[i] != "--order" {
+						ele = insert(ele, args[i])
+					}
+				}
+				if args[0][0:3] == "--i" {
+					ele = insert(ele, args[0][9:])
+				} else {
+					ele = insert(ele, args[0][3:])
+				}
+				order(ele)
 			}
+		} else {
+			fmt.Println(args[0])
 		}
-		if count == 0 {
-			sorted = true
-		}
+	} else {
+		help()
 	}
 }
