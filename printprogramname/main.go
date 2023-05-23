93c015b927eb30961e0a584656a4e2f70888558c
fatabbas <fatima77614@gmail.com>
2023-05-23 15:02:50 +0300

    Q.6, pt.1

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,12 +1,13 @@
 package main
 
-import ( "github.com/01-edu/z01"
-"os" )
+import ( 
+	"os"
+	"github.com/01-edu/z01"
+ )
 
 func main() {
 	wee := os.Args
 	z01.PrintRune(len(wee))
-
     names := os.Args[1:]
 	z01.PrintRune(reflect.TypeOf(names))
 }
