e1f18cbacee42e7977e17d35bbe60cecb2331362
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-23 12:41:31 +0000

    Update 'printprogramname/main.go'

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -10,8 +10,9 @@ func main() {
 	wee := os.Args[0]
 	runes := []rune(wee)
 	for _, word := range runes {
-		for _, word2 := range word[2:] {
-			z01.PrintRune(word2)
+		if word == '.' || word == '/' {
+		} else {
+			z01.PrintRune(word)
 		}
 	}
 	z01.PrintRune('\n')
