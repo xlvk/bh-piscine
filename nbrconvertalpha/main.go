e9f0d084acdcea2f876af6c71c3e0eb8a27d00a3
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-23 21:03:47 +0000

    Update 'nbrconvertalpha/main.go'

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -2,53 +2,36 @@ package main
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/01-edu/z01"
 )
 
 func main() {
-	arguments := os.Args[1:]
-	caps := false
+	args := os.Args[1:]
+	upperCase := false
 
-	if len(arguments) > 0 && arguments[0] == "--upper" {
-		caps = true
-		arguments = arguments[1:]
+	if len(args) > 0 && args[0] == "--upper" {
+		upperCase = true
+		args = args[1:]
 	}
 
-	for _, arg := range arguments {
-		if caps {
-			z01.PrintRune(toUpper(arg))
+	for i, arg := range args {
+		num, err := strconv.Atoi(arg)
+		if err != nil || num < 1 || num > 26 {
+			z01.PrintRune(' ')
 		} else {
-			z01.PrintRune(toLower(arg))
+			if upperCase {
+				z01.PrintRune(rune('A' + num - 1))
+			} else {
+				z01.PrintRune(rune('a' + num - 1))
+			}
+		}
+
+		if i < len(args)-1 {
+			z01.PrintRune(' ')
 		}
 	}
+
 	z01.PrintRune('\n')
-}
-
-func toLower(arg string) rune {
-	numv := 0
-	for _, c := range arg {
-		if c < '0' || c > '9' {
-			return ' '
-		}
-		numv = numv*10 + int(c-'0')
-	}
-	if numv >= 1 && numv <= 26 {
-		return rune(numv - 1 + 'a')
-	}
-	return ' '
-}
-
-func toUpper(arg string) rune {
-	numv := 0
-	for _, c := range arg {
-		if c < '0' || c > '9' {
-			return ' '
-		}
-		numv = numv*10 + int(c-'0')
-	}
-	if numv >= 1 && numv <= 26 {
-		return rune(numv - 1 + 'A')
-	}
-	return ' '
 }
