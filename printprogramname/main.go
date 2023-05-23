e578adacdad84215bc85f2ef8674856295a18601
fatabbas <fatima77614@gmail.com>
2023-05-23 16:14:45 +0300

    fx

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -9,10 +9,16 @@ import (
 func main() {
 	wee := os.Args[0]
 	runes := []rune(wee)
+<<<<<<< HEAD
 	for _, word := range runes {
 		if word == '.' || word == '/' {
 		} else {
 			z01.PrintRune(word)
+=======
+	for i, word := range runes {
+		if i != 0 && i != 1 {
+			z01.PrintRune(rune(word))
+>>>>>>> 3bc26dd (Q.6, pt.1)
 		}
 	}
 	z01.PrintRune('\n')
