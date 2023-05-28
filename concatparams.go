9fe47d54073cc2f984bdf1ee43d54a965da03b9a
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-28 12:38:27 +0000

    concatparams

---
+++ concatparams.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	if len(args) == 0 {
+		return ""
+	}
+	c := " "
+	for i := 0; i < len(args); i++ {
+		c += args[i]
+		if i < len(args)-1 {
+			c += "\n"
+		}
+	}
+	return c
+}
