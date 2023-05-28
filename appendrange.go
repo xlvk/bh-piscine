18911632eef6dcc7b645a320defac92a9695fc9e
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-28 11:30:25 +0000

    Update 'appendrange.go'

--- appendrange.go
+++ appendrange.go
@@ -1,9 +1,10 @@
 package piscine
 
 func AppendRange(min, max int) []int {
+	size := max - min + 1
 	var myArray []int
-	for i := min; i <= max; i++ {
-		myArray = append(myArray, i)
+	for i := 0; i < size; i++ {
+		myArray[i] = min + i
 	}
 	return myArray
 }
