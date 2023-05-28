28472371c9cd145254ecca66cbb7e209dabb2a6a
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-28 23:43:36 +0000

    split

--- split.go
+++ split.go
@@ -4,8 +4,8 @@ func Split(s, sep string) []string {
 	index := 0
 	var o string
 	var wee []string
-	for i := 0; i < len(s); i++ {
-		if s[i] == sep[0] && s[i+1] == sep[1] {
+	for i := 0; i < len(s)-len(sep)+1; i++ {
+		if s[i:i+len(sep)] == sep {
 			o = s[index:i]
 			wee = append(wee, o)
 			o = ""
