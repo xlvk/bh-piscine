4f90bd30256be1004a8a7620a12d2a4f1830b87c
fatabbas <fatima77614@gmail.com>
2023-05-28 19:38:16 +0300

    splitwhitespace

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,16 +4,21 @@ func SplitWhiteSpaces(s string) []string {
 	var wee []string
 	var o string
 	index := 0
+	mm := 0
 	for i := 0; i < len(s); i++ {
 		if s == " " || i == len(s)-1 {
 			if i == len(s)-1 {
 				o = s[index : i+1]
+				mm++
 			} else {
 				o = s[index:i]
+				mm++ 
 			}
-			wee = append(wee, o)
-			o = ""
-			index = i + 1
+			for k : = 0; k < mm; k++ {
+				wee = append(wee, o)
+				o = ""
+				index = i + 1
+			}
 		}
 	}
 	return wee
