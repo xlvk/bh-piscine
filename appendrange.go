b087d52e0ef7a883f5d2999cf95940b49744426b
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-28 11:23:11 +0000

    Upload files to ''

---
+++ appendrange.go
@@ -0,0 +1,11 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	size := max - min + 1
+	myArray := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		myArray[i] = min + i
+	}
+	return myArray
+}
