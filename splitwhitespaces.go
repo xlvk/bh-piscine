f6fd9c24ab4cea7abea6155feb3a9e7ff2a5cd47
fatabbas <fatima77614@gmail.com>
2023-05-28 18:39:22 +0300

    splitwhitespace

---
+++ splitwhitespaces.go
@@ -0,0 +1,21 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	var wee []string
+	var o string
+	woo := s + " "
+	if len(s) == 0 {
+		return ""
+	}
+	for _, v := range woo {
+		if v == ' ' || v == '  ' || c == '\n' {
+			if o != "" {
+				wee = append(wee, o)
+				o = ""
+			}
+		} else {
+			o += string(v)
+		}
+	}
+	return wee
+}
