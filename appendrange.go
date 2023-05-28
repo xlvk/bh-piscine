89784b0248d493f2334d227121df9ff9e6ebecc5
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-28 11:59:33 +0000

    Update 'appendrange.go'

--- appendrange.go
+++ appendrange.go
@@ -5,7 +5,7 @@ func AppendRange(min, max int) []int {
 		return nil
 	}
 	var myArray []int
-	for i := min; i <= max; i++ {
+	for i := min; i < max; i++ {
 		myArray = append(myArray, i)
 	}
 	return myArray
