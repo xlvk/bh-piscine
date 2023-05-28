fa5f7b3e02c0932923a6fffb29b54bb8a509052a
fatabbas <fatima77614@gmail.com>
2023-05-28 19:16:43 +0300

    splitwhitespace

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -5,7 +5,7 @@ func SplitWhiteSpaces(s string) []string {
 	var o string
 	index := 0
 	for i := 0; i < len(s); i++ {
-		if s == ' ' || i == len(s)-1 {
+		if s == " " || i == len(s)-1 {
 			if i == len(s)-1 {
 				o = s[index : i+1]
 			} else {
