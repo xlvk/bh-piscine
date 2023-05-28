f7ec842672cece291e073f775887e608abe6feb1
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-28 22:48:56 +0000

    printwordstables

--- printwordstables.go
+++ printwordstables.go
@@ -4,7 +4,26 @@ import "github.com/01-edu/z01"
 
 func PrintWordsTables(a []string) {
 	for _, word := range a {
-		PrintString(word)
-		PrintRune('\n')
+		for _, r := range word {
+			z01.PrintRune(r)
+		}
+		z01.PrintRune('\n')
 	}
+}
+
+func SplitWhiteSpaces(s string) []string {
+	var words []string
+	word := ""
+	for i := 0; i < len(s); i++ {
+		if s[i] != ' ' {
+			word += string(s[i])
+		} else if word != "" {
+			words = append(words, word)
+			word = ""
+		}
+	}
+	if word != "" {
+		words = append(words, word)
+	}
+	PrintWordsTables(words)
 }
