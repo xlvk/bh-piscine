db6a1acd6dec39a0abddc2e9701988eb6f7ef811
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-28 22:39:16 +0000

    printwordstables

--- printwordstables.go
+++ printwordstables.go
@@ -1,20 +1,10 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
 func PrintWordsTables(a []string) {
-	var words []string
-	weee := len(a)
-	word := ""
-	for i := 0; i < weee; i++ {
-		if a[i] != " " {
-			word += string(a[i])
-		} else if word != "" {
-			words = append(words, word)
-			word = ""
-		}
+	for _, word := range a {
+		PrintString(word)
+		PrintRune('\n')
 	}
-	if word != "" {
-		words = append(words, word)
-	}
-	a = words
-	return a
 }
