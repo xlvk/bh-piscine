ab3450cb337c53b31ac1810df5cc889bcc697f55
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-28 12:04:32 +0000

    makerange

---
+++ makerange.go
@@ -0,0 +1,13 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	if min > max {
+		return nil
+	}
+	size := max - min
+	myArray := make([]int, size+1)
+	for i := min; i < max; i++ {
+		myArray[i] = min + i
+	}
+	return myArray
+}
