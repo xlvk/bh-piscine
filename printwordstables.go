604dae9182efad35f21ecde4e04d7f0138690f5a
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-28 22:50:22 +0000

    printwordstables

--- printwordstables.go
+++ printwordstables.go
@@ -10,20 +10,3 @@ func PrintWordsTables(a []string) {
 		z01.PrintRune('\n')
 	}
 }
-
-func SplitWhiteSpaces(s string) []string {
-	var words []string
-	word := ""
-	for i := 0; i < len(s); i++ {
-		if s[i] != ' ' {
-			word += string(s[i])
-		} else if word != "" {
-			words = append(words, word)
-			word = ""
-		}
-	}
-	if word != "" {
-		words = append(words, word)
-	}
-	PrintWordsTables(words)
-}
