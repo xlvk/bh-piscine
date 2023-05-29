0cedae1b4d013b8332ea9a4b32b24030b83343ce
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-29 00:23:02 +0000

    Upload files to 'boolean'

---
+++ boolean/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if nbr%2 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	lengthOfArg := 0
+	// a := 1
+	// b := 2
+	EvenMsg := "I have an even number of arguments"
+	OddMsg := "I have an odd number of arguments"
+	for range os.Args[1:] {
+		lengthOfArg++
+	}
+	if isEven(lengthOfArg) {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
