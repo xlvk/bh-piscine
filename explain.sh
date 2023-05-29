4eef1b0e32a5cfdb710b68f21eb35568e770453d
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-29 21:34:14 +0000

    Update 'explain.sh'

--- explain.sh
+++ explain.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+
+# Display the first and last name of the key witness
+echo "FirstNameOfWitness LastNameOfWitness"
+
+# Display the interview number of the witness
+echo "123456"
+
+# Display the color and make of the car of the main suspect
+echo "Red Ferrari"
+
+# Display the names of the 3 other main suspects in alphabetical order of their last name
+echo "FirstNameOfSuspect1 LastNameOfSuspect1"
+echo "FirstNameOfSuspect2 LastNameOfSuspect2"
+echo "FirstNameOfSuspect3 LastNameOfSuspect3"
+
 chmod +x explain.sh
+
+./explain.sh | cat -e
