d69efb5932e4ff9a30cb9acb8037d68ab6d72bb0
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-29 19:58:36 +0000

    Update 'convertbase.go'

--- convertbase.go
+++ convertbase.go
@@ -4,7 +4,7 @@ func ConvertBase(nbr, baseFrom, baseTo string) string {
 	decimal := 0
 	// Convert the number from baseFrom to decimal
 	for i := 0; i < len(nbr); i++ {
-		decimal = decimal*len(baseFrom) + int(nbr[i]-'0')
+		decimal = decimal*len(baseFrom) + indexOf(baseFrom, nbr[i])
 	}
 	// Convert the decimal number to baseTo
 	result := ""
@@ -13,6 +13,15 @@ func ConvertBase(nbr, baseFrom, baseTo string) string {
 		result = string(baseTo[remainder]) + result
 		decimal = decimal / len(baseTo)
 	}
-
 	return result
+}
+
+// Helper function to get the index of a character in a string
+func indexOf(str string, char byte) int {
+	for i := 0; i < len(str); i++ {
+		if str[i] == char {
+			return i
+		}
+	}
+	return -1
 }
