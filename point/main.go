974b6a00553e9d164d7b051f896d0c7a0bf7774d
fatabbas <fatima77614@gmail.com>
2023-05-29 18:46:30 +0300

    splitwhitespacsrgzdefaee

--- point/main.go
+++ point/main.go
@@ -23,26 +23,8 @@ func main() {
 	points := &point{}
 	setPoint(points)
 	PrintStr("x = ")
-	SolveNbr(points.x)
+	PrintStr((points.x - '0'))
 	PrintStr(", y = ")
-	SolveNbr(points.y)
+	PrintStr((points.y - '0'))
 	PrintStr("\n")
-}
-
-func SolveNbr(n int) {
-	b := 1
-	if n < 0 {
-		b = -1
-		z01.PrintRune('-')
-	}
-	if n != 0 {
-		c := (n / 10) * b
-		if c != 0 {
-			SolveNbr(c)
-		}
-		wee := (n % 10 * b) + '0'
-		z01.PrintRune(rune(wee))
-	} else {
-		z01.PrintRune('0')
-	}
 }
