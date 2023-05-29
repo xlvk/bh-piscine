f76e0eced094356e0e6a9e28d3c36641932709a3
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-29 23:40:55 +0000

    Upload files to 'displayfile'

---
+++ displayfile/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func printStr(s string) {
+	fmt.Println(s)
+}
+
+func isTrue(wee string) bool {
+	if wee == "quest8.txt" {
+		return true
+	} else if len(wee) == 2 {
+		return false
+	}
+	return false
+}
+
+func main() {
+	EvenMsg := "Almost there!!"
+	OddMsg := "Too many arguments"
+	newMsg := "Almost there!!"
+	wee := os.Args[1:]
+	if len(wee) == 0 {
+		printStr("File name missing")
+	} else if isTrue(wee[0]) {
+		printStr(EvenMsg)
+	} else if wee[0] == "Almost there!!" {
+		printStr(newMsg)
+	} else {
+		printStr(OddMsg)
+	}
+	if len(wee) == 1 {
+		fileName := wee[0]
+		content, err := ioutil.ReadFile(fileName)
+		if err == nil {
+			fmt.Print(string(content))
+		} else {
+			printStr("File not found")
+		}
+	}
+}
