e93d234c76f5d6687df8f411e13c01961c985fbd
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-29 01:10:22 +0000

    Update 'point/main.go'

--- point/main.go
+++ point/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	
 	"github.com/01-edu/z01"
 )
 
@@ -17,8 +15,40 @@ func setPoint(ptr *point) {
 
 func main() {
 	points := &point{}
-
 	setPoint(points)
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	SolveNbr(points.x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	SolveNbr(points.y)
+	z01.PrintRune('\n')
+}
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+func SolveNbr(n int) {
+	b := 1
+	if n < 0 {
+		b = -1
+		z01.PrintRune('-')
+		n = -n
+	}
+	if n != 0 {
+		digits := make([]int, 0)
+		for n > 0 {
+			digit := n % 10
+			digits = append(digits, digit)
+			n /= 10
+		}
+		for i := len(digits) - 1; i >= 0; i-- {
+			z01.PrintRune('0' + digits[i]*b)
+		}
+	} else {
+		z01.PrintRune('0')
+	}
 }
