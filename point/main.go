141c2f09a7169ad56fc932862bb2adf5dba08dcb
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-29 19:33:51 +0000

    Update 'point/main.go'

--- point/main.go
+++ point/main.go
@@ -1,62 +1,49 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 type point struct {
 	x, y int
 }
 
-func setPoint(ptr *point) {
-	ptr.x = 42
-	ptr.y = 21
+func main() {
+	points := &point{}
+	setPoint(points)
+
+	printStr("x = ")
+	printNumber(points.x)
+	printStr(", y = ")
+	printNumber(points.y)
+	z01.PrintRune('\n')
 }
 
-func PrintStr(s string) {
+func printStr(s string) {
 	for _, ch := range s {
 		z01.PrintRune(ch)
 	}
 }
 
-func main() {
-	points := &point{}
-	setPoint(points)
-	PrintStr("x = ")
-	n := points.x
-	if n < 0 {
-		PrintStr("-")
-		n = -n
-	}
-	printDigits(n)
-	PrintStr(", y = ")
-	b := 1
-	n = points.y
-	if n < 0 {
-		b = -1
-		PrintStr("-")
-		n = -n
-	}
-	printDigits(n)
-	z01.PrintRune('\n')
-}
-
-func printDigits(n int) {
+func printNumber(nbr int) {
 	digits := [20]int{}
 	i := 0
-	for n > 0 {
-		digit := n % 10
+
+	for nbr > 0 {
+		digit := nbr % 10
 		digits[i] = digit
-		n /= 10
+		nbr /= 10
 		i++
 	}
-	power := 1
+
 	for j := i - 1; j >= 0; j-- {
-		digit := digits[j]
-		for k := 0; k < power; k++ {
-			digit *= 10
+		index := '0'
+		for k := 0; k < digits[j]; k++ {
+			index++
 		}
-		z01.PrintRune('0' + digit)
-		power *= 10
+		z01.PrintRune(index)
 	}
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
 }
