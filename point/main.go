d2a0f90201fbaf2484ec927523be4d49c6f7a2d0
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-29 19:26:00 +0000

    Update 'point/main.go'

--- point/main.go
+++ point/main.go
@@ -23,8 +23,40 @@ func main() {
 	points := &point{}
 	setPoint(points)
 	PrintStr("x = ")
-	PrintStr((points.x - '0'))
+	n := points.x
+	if n < 0 {
+		z01.PrintRune('-')
+		n = -n
+	}
+	printDigits(n)
 	PrintStr(", y = ")
-	PrintStr((points.y - '0'))
-	PrintStr("\n")
+	b := 1
+	n = points.y
+	if n < 0 {
+		b = -1
+		z01.PrintRune('-')
+		n = -n
+	}
+	printDigits(n)
+	z01.PrintRune('\n')
+}
+
+func printDigits(n int) {
+	digits := [20]int{}
+	i := 0
+	for n > 0 {
+		digit := n % 10
+		digits[i] = digit
+		n /= 10
+		i++
+	}
+	power := 1
+	for j := i - 1; j >= 0; j-- {
+		digit := digits[j]
+		for k := 0; k < power; k++ {
+			digit *= 10
+		}
+		z01.PrintRune('0' + digit)
+		power *= 10
+	}
 }
