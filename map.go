a19e86374063059a480399295196419b9d15cf7a
Fatima Abbas <fatabbas@desk-0023.reboot.internal>
2023-05-30 12:05:14 +0300

    map.go

---
+++ map.go
@@ -0,0 +1,19 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	if nb <= 1 {
+		return false
+	}
+	for i := 2; i*i <= nb; i++ {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func Map(f func(int) bool, a []int) []bool {
+	for _, i := range a {
+		f(i)
+	}
+}
