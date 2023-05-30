13ee34db403b56364fb2bc76c47481c13b303703
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-30 09:29:40 +0000

    Update 'map.go'

--- map.go
+++ map.go
@@ -1,39 +1,21 @@
 package piscine
 
-func IsPrime(num int) bool {
-	if num == 1 || num == 0 {
+func IsPrime(nb int) bool {
+	if nb <= 1 {
 		return false
 	}
-
-	// Check divisibility up to the square root of the number
-	limit := intSqrte(num)
-	for i := 2; i <= limit; i++ {
-		if num%i == 0 {
+	for i := 2; i*i <= nb; i++ {
+		if nb%i == 0 {
 			return false
 		}
 	}
 	return true
 }
 
-func intSqrte(num int) int {
-	// Use Newton's method to find the integer square root
-	x := num
-	y := (x + 1) / 2
-	for y < x {
-		x = y
-		y = (x + num/x) / 2
-	}
-	return x
-}
-
 func Map(f func(int) bool, a []int) []bool {
-	m := make(map[int]bool)
-	for _, i := range a {
-		m[i] = f(i)
+	result := make([]bool, len(a))
+	for i, val := range a {
+		result[i] = f(val)
 	}
-	wee := make([]bool, len(m))
-	for o, j := range wee {
-		wee[o] = j
-	}
-	return wee
+	return result
 }
