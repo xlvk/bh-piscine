2fecb714f43fea0793e45df7c78826e0800198e2
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-30 12:46:55 +0000

    Update 'issorted.go'

--- issorted.go
+++ issorted.go
@@ -1,14 +1,10 @@
 package piscine
 
-func IsSorted(f func(a, b int) int, a []int) bool {
+func IsSorted(a []int) bool {
 	for i := 1; i < len(a); i++ {
-		if f(a[i-1], a[i]) > 0 {
+		if a[i-1] > a[i] {
 			return false
 		}
 	}
 	return true
-}
-
-func isSortedBy10(a, b int) int {
-	return (a / 10) - (b / 10)
 }
