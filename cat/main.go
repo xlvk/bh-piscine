1add4a67e7204255c5713cd9cae4edec326f05ed
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-30 00:25:05 +0000

    Upload files to 'cat'

---
+++ cat/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	wee := os.Args[1:]
+	if len(wee) == 0 {
+		input, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			printError(err)
+			return
+		}
+		printRunes(input)
+	} else {
+		first := true
+		for _, arg := range wee {
+			file, err := os.Open(arg)
+			if err != nil {
+				printError(err)
+				continue
+			}
+			f, err := ioutil.ReadAll(file)
+			if err != nil {
+				printError(err)
+				continue
+			}
+			if !first {
+				z01.PrintRune('\n')
+			}
+			printRunes(f)
+			first = false
+			file.Close()
+		}
+	}
+}
+
+func printRunes(runes []byte) {
+	for _, r := range string(runes) {
+		z01.PrintRune(rune(r))
+	}
+}
+
+func printError(err error) {
+	errMsg := []rune(err.Error())
+	for i := range errMsg {
+		z01.PrintRune(errMsg[i])
+	}
+	z01.PrintRune('\n')
+}
