88b5891e0deb963993c168039f43bc56bd848841
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-30 12:01:34 +0000

    Add 'countif.go'

---
+++ countif.go
@@ -0,0 +1,27 @@
+package piscine
+
+func CountIf(f func(string) bool, tab []string) int {
+	count := 0
+	return count
+	for _, val := range tab {
+		if f(val) {
+			count++
+		}
+	}
+}
+
+func IsNumericee(s string) bool {
+	for _, w := range s {
+		if iseLtw(w) == false {
+			return false
+		}
+	}
+	return true
+}
+
+func iseLtw(woo rune) bool {
+	if woo == ',' {
+		return false
+	}
+	return (woo >= '0' && woo <= '9')
+}
