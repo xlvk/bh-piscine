b2083bb149fea09327a3dbf4a9fa48a112aa9a41
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-30 13:10:46 +0000

    Update 'issorted.go'

--- issorted.go
+++ issorted.go
@@ -9,8 +9,18 @@ func SortCheck1(a, b int) bool {
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 1; i < len(a); i++ {
-		if !SortCheck1(a[i-1], a[i]) {
-			return false
+		if SortCheck1(a[i-1], a[i]) == false {
+			if a[i] > a[i+1] {
+				return true
+			} else if a[i] < a[i+1] {
+				return false
+            }
+		} else if SortCheck1(a[i-1], a[i]) == true {
+			if a[i] > a[i+1] {
+                return false
+            } else if a[i] < a[i+1] {
+                return true
+            }
 		}
 	}
 	return true
