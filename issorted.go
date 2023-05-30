029ab5d8564af7b86876085d59e7aa2e626bbcb8
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-30 12:40:20 +0000

    Update 'issorted.go'

--- issorted.go
+++ issorted.go
@@ -1,16 +1,5 @@
 package piscine
 
-func SortIntegerTablee(table []int) {
-	n := len(table)
-	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-i-1; j++ {
-			if table[j] > table[j+1] {
-				table[j], table[j+1] = table[j+1], table[j]
-			}
-		}
-	}
-}
-
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 1; i < len(a); i++ {
 		if f(a[i-1], a[i]) > 0 {
