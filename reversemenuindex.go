3f56ff813154777947d19fca853dade0b0552340
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-31 22:05:37 +0000

    Update 'reversemenuindex.go'

--- reversemenuindex.go
+++ reversemenuindex.go
@@ -2,12 +2,12 @@ package piscine
 
 func ReverseMenuIndex(menu []string) []string {
 	wewee := len(menu)
-	ha? := make([]string, wewee)
+	haa := make([]string, wewee)
 
 	for i, n := range menu {
-		j := wewee - i - 1
-
-		ha?[j] = n
+		for j := wewee - 1; j <= 0; j-- {
+			haa[j] = n
+		}
 	}
-	return ha?
+	return haa
 }
