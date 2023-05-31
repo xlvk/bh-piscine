d77a1172fee8f58930418b1f6ebb816ffc5cbcdc
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-31 16:20:17 +0000

    Update 'unmatch.go'

--- unmatch.go
+++ unmatch.go
@@ -1,36 +1,26 @@
 package piscine
 
 func Unmatch(a []int) int {
-	me := 0
+	me := -1
 	if len(a) == 0 {
-		return me
+		return 0
 	}
 	if len(a) == 1 {
 		return a[0]
 	}
-	if len(a) == 2 {
-		if a[0] != a[1] {
-			return me
+	for i := 0; i < len(a); {
+		for j := i + 1; j <= len(a)-1; j++ {
+			if !checkMatch(a[i], a[j]) {
+				me = a[i]
+			}
 		}
-		return a[0]
-	}
-	if len(a) == 3 {
-		if a[0] != a[1] || a[0] != a[2] {
-			return me
-		}
-		return a[0]
-	}
-	if len(a) == 4 {
-		if a[0] != a[1] || a[0] != a[2] || a[0] != a[3] {
-			return me
-		}
-		return a[0]
-	}
-	if len(a) == 5 {
-		if a[0] != a[1] || a[0] != a[2] || a[0] != a[3] || a[0] != a[4] {
-			return me
-		}
-		return a[0]
 	}
 	return me
+}
+
+func checkMatch(a, b int) bool {
+	if a == b {
+		return true
+	}
+	return false
 }
