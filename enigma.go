b8ff28cefa323b2af01c7f680f6eaf1ff5af5f25
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-31 18:16:02 +0000

    Update 'enigma.go'

--- enigma.go
+++ enigma.go
@@ -1,9 +1,9 @@
 package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	tab := a***
-	***a := *******c
-    *******c := ***d
-	***d := *b
-	*b := tab
+	tab := ***a
+	***a = *******c
+	*******c = ***d
+	***d = *b
+	*b = tab
 }
