aed9668b504a19eef46155a9e323b4d5e2971a91
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-31 19:29:59 +0000

    Update 'descendappendrange.go'

--- descendappendrange.go
+++ descendappendrange.go
@@ -4,8 +4,9 @@ func DescendAppendRange(max, min int) []int {
 	if max <= min {
 		if min == 0 && max == 0 {
 			return nil
+		} else {
+			return []int{}
 		}
-		return []int{}
 	}
 	var myArray []int
 	for i := max; i > min; i-- {
