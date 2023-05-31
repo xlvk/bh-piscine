b68afb09ca1b85aa610d666164f6fa3ec5a0417f
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-31 19:25:56 +0000

    Update 'descendappendrange.go'

--- descendappendrange.go
+++ descendappendrange.go
@@ -2,10 +2,10 @@ package piscine
 
 func DescendAppendRange(max, min int) []int {
 	if max <= min {
-		if min != 0 || max != 0 {
-			return []int{}
+		if min == 0 && max == 0 {
+			return nil
 		}
-		return nil
+		return []int{}
 	}
 	var myArray []int
 	for i := max; i > min; i-- {
