6eef4bd1ecac010e3f5e88c467eed94b1c95c788
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-05-31 09:05:28 +0000

    Update 'rot14.go'

--- rot14.go
+++ rot14.go
@@ -7,7 +7,7 @@ func Rot14(s string) string {
 			resulte += " "
 		} else if r >= 'A' && r <= 'L' || r >= 'a' && r <= 'l' {
 			resulte += string(r + 14)
-		} else if r >= 'M' && r <= 'Z' || r >= 'n' && r <= 'z' {
+		} else if r >= 'M' && r <= 'Z' || r >= 'm' && r <= 'z' {
 			resulte += string(r - 12)
 		} else {
 			resulte += string(r)
