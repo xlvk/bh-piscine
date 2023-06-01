606d241a6dd3a8bec8322218f6e6c91bbb8163bf
fatabbas <fatima77614@gmail.com>
2023-06-01 11:57:12 +0300

    collatzcountdown

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -5,8 +5,8 @@ func CollatzCountdown(start int) int {
 	if start <= 0 {
 		return -1
 	}
-	for start!= 1 {
-		if start %2 == 0 {
+	for start != 1 {
+		if start%2 == 0 {
 			start = start / 2
 		} else {
 			start = start*3 + 1
