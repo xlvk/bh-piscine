137237f493616aea7923c7931031991d7ab1f477
fatabbas <fatima77614@gmail.com>
2023-06-01 11:52:24 +0300

    collatzcountdown

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -1,14 +1,17 @@
 package piscine
 
 func CollatzCountdown(start int) int {
-	wow := start
+	wow := 0
 	if start <= 0 {
 		return -1
-	} else {
-		if start%2 == 0 {
-			return wow / 2
-		} else {
-			return (3 * wow) + 1
-		}
 	}
+	for start!= 1{
+		if start %2 == 0{
+			start = start /2
+		} else{
+			start = start * 3 + 1
+		}
+		wow++
+	}
+	return wow
 }
