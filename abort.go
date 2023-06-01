4eb764bb1b6f79a18cf05d6170a0bc09250bf038
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-06-01 10:44:12 +0000

    Update 'abort.go'

--- abort.go
+++ abort.go
@@ -1,8 +1,8 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	wee := []int{a , b , c , d , e}
-	for i := 0; i < 4; i ++ {
+	wee := []int{a, b, c, d, e}
+	for i := 0; i < 4; i++ {
 		for j := 1; j < 5; j++ {
 			if wee[i] < wee[j] {
 				haha := wee[j]
@@ -11,6 +11,6 @@ func Abort(a, b, c, d, e int) int {
 			}
 		}
 	}
-	ha := wee[2] 
+	ha := wee[2]
 	return ha
 }
