1971bdcc8120875207ad9582a9caf43fa6437942
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-06-07 15:23:49 +0000

    Add 'listreverse.go'

---
+++ listreverse.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ListReverse(l *List) {
+	current := l.Head
+	var prev *NodeL
+	prev = nil
+
+	for current != nil {
+		next := current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+	l.Head = prev
+}
