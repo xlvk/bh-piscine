43b70fe1965157daf47b770d3c728bdf48274bb6
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-06-07 15:14:47 +0000

    Update 'listpushback.go'

--- listpushback.go
+++ listpushback.go
@@ -1,22 +1,22 @@
 package piscine
 
-type NodeL1 struct {
+type NodeL struct {
 	Data interface{}
 	Next *NodeL
 }
 
-type List1 struct {
+type List struct {
 	Head *NodeL
 	Tail *NodeL
 }
 
 func ListPushBack(l *List, data interface{}) {
 	n := &NodeL{Data: data}
+
 	if l.Head == nil {
 		l.Head = n
-		l.Tail = n
 	} else {
 		l.Tail.Next = n
-		l.Tail = l.Tail.Next
 	}
+	l.Tail = n
 }
