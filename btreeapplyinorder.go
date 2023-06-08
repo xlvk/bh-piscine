427cc02cdae094ccb4c2d1ea6e6e063bb1dcf92c
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-06-08 21:20:17 +0000

    Update 'btreeapplyinorder.go'

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -1,10 +1,17 @@
 package piscine
 
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
 func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
-	if root == nil {
-		return
+	if root != nil {
+
+		BTreeApplyInorder(root.Left, f)
+		f(root.Data)
+		BTreeApplyInorder(root.Right, f)
+
 	}
-	BTreeApplyInorder(root.Left, f)
-	f(root.Data)
-	BTreeApplyInorder(root.Right, f)
+
 }
