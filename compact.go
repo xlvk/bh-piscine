7288ed5a11836b4ebb3997ee336d304a11a6bdbc
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-06-25 07:57:34 +0000

    Update 'compact.go'

--- compact.go
+++ compact.go
@@ -2,13 +2,13 @@ package piscine
 
 func Compact(ptr *[]string) int {
     	count := 0
-        	for _, e := range *ptr {
-                		if e != "" {
-                            			(*ptr)[count] = e
-                                        			count++
-                        }
+        for _, e := range *ptr {
+    		if e != "" {
+				(*ptr)[count] = e
+                count++
             }
+        }
 
-            	*ptr = (*ptr)[:count]
-                	return count
+        *ptr = (*ptr)[:count]
+        return count
 }
