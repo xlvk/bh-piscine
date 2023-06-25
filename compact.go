465381623760b6c20ede7dd7fc460c72c72a28b3
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-06-25 07:20:19 +0000

    Add 'compact.go'

---
+++ compact.go
@@ -0,0 +1,17 @@
+package piscine
+
+func Compact(ptr *[]string) int {
+    	count := 0
+        	for _, e := range *ptr {
+                		if e != "" {
+                            			(*ptr)[count] = e
+                                        			count++
+                        }
+            }
+
+            	*ptr = (*ptr)[:count]
+                	return count
+}
+                        }
+            }
+}
