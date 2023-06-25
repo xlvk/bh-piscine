2972817ebef5c829accf21ceacd36ee6e4d2b0ca
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-06-25 07:58:40 +0000

    Update 'compact.go'

--- compact.go
+++ compact.go
@@ -4,11 +4,10 @@ func Compact(ptr *[]string) int {
     	count := 0
         for _, e := range *ptr {
     		if e != "" {
-				(*ptr)[count] = e
+                (*ptr)[count] = e
                 count++
             }
         }
-
         *ptr = (*ptr)[:count]
         return count
 }
